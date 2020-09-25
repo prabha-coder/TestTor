d2f9438653bf330f8e6a31f9454cd38cb90b06a5
PRABAKAR <58840015+prabha-coder@users.noreply.github.com>
2020-09-25 23:05:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ app.get('/', (req, res) => {
 <label for="secret">Enter your aria2 secret:</label>
 <input id="secret" type="password">
 <button id="panel">Go to AriaNg panel</button>
-<button id="downloads">View downloaded files</button><br><br>
-<a href="https://drive.google.com/drive/folders/0AOHzWjdqLe4yUk9PVA" target="_blank"> Check Your File --> Gdrive</a>
+<button id="downloads">View downloaded files</button>
 <script>
 panel.onclick=function(){
 	open('/ariang/#!/settings/rpc/set/wss/'+location.hostname+'/443/jsonrpc/'+btoa(secret.value),'_blank')
